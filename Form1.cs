2727aa73beda05f071548e1d6aa5aad3b2475605
A601-22 <tnk.dat.class@gmail.com>
2021-06-17 09:57:29 +0900

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace v0617
+{
+    public partial class Form1 : Form
+    {
+        int vx = -10;
+        int vy = -10;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            label1.Top += vy;
+
+           
+            if (label1.Top < 0)
+                vy += Math.Abs(vy);
+            
+               
+            if (label1.Left < 0)
+                vx += Math.Abs(vx);
+           
+                vx += -Math.Abs(vx);
+        }
+    }
+}
